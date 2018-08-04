3dbff5ce3f0332b2291c5448d9f15017450c6243
Dwi Rachmat Putra <awawaacute@gmail.com>
2018-08-04 19:55:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '@everyone mabar') {
+    	message.reply('kuyy');
   	}
 });
 
