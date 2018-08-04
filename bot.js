d9453409f5b316fd105f6191dfadfe8d580698c5
Dwi Rachmat Putra <awawaacute@gmail.com>
2018-08-04 19:39:48 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,4 +13,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-bot.user.setPresence({ activity: { name: 'with discord.js' }, status: 'idle' });
+
